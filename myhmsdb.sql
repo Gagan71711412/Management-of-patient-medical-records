81a43cd9011a33ee594580327db377f82c78cc14
Gagan71711412 <149614505+Gagan71711412@users.noreply.github.com>
2024-01-05 02:54:43 +0530

    Update myhmsdb.sql

--- myhmsdb.sql
+++ myhmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 16, 2020 at 02:34 AM
+-- Generation Time: November 16, 2023 at 04:34 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
